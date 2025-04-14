c65f5bede03ab287a731694079497f54d1a65409
euFernandoAmorim <fernando.amorim@sptech.school>
2025-04-13 23:18:59 -0300

    Inicio da documentação Swagger

--- API_CRUDBackend/src/main/java/school/sptech/CRUDBackend/controller/CorteTecidoController.java
+++ API_CRUDBackend/src/main/java/school/sptech/CRUDBackend/controller/CorteTecidoController.java
@@ -1,5 +1,9 @@
 package school.sptech.CRUDBackend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,12 +16,24 @@ import school.sptech.CRUDBackend.service.CorteTecidoService;
 
 import java.util.List;
 
+@Tag(name = "Cortes de Tecidos", description = "Operações CRUD relacionadas aos cortes de tecidos (data de inicio, término do corte e quem efetuou).")
 @RestController
 @RequestMapping("/cortes-tecido")
 @RequiredArgsConstructor
 public class CorteTecidoController {
     private final CorteTecidoService corteTecidoService;
 
+    @Operation(
+            summary = "Cadastramento de um novo corte de tecido.",
+            description = "Retorna um objeto do tipo CorteTecidoResponseDto quando cadastrado com sucesso."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Corte cadastrado com sucesso.")
+    })
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "Objeto do tipo CorteTecidoRequestDto para cadastro.",
+            required = true
+    )
     @PostMapping
     public ResponseEntity<CorteTecidoResponseDto> cadastrar(
             @RequestBody @Valid CorteTecidoRequestDto corteTecido
@@ -29,6 +45,12 @@ public class CorteTecidoController {
         return ResponseEntity.status(201).body(corteTecidoCadastrado);
     }
 
+
+    @Operation(summary = "Listagem de todos os cortes de tecidos no sistema.", description = "Retorna uma lista de CorteTecidoResponseDtoa com todos os cortes de tecido no sistema.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "lista possui cortes de tecido"),
+            @ApiResponse(responseCode = "204", description = "lista de cortes está vazia"),
+    })
     @GetMapping
     public ResponseEntity<List<CorteTecidoResponseDto>> listarTodos() {
         List<CorteTecidoResponseDto> cortesTecido = CorteTecidoMapper.toResponseDtos(
@@ -40,6 +62,12 @@ public class CorteTecidoController {
         return ResponseEntity.status(200).body(cortesTecido);
     }
 
+
+    @Operation(summary = "Exibição de um corte de tecido por ID", description = "Retorna um objeto CorteTecidoResponseDto de acordo com o ID informado na PathVariable.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Registro encontrado com sucesso."),
+            @ApiResponse(responseCode = "404", description = "Nenhum registro com o ID informado no PathVariable foi encontrado."),
+    })
     @GetMapping("/{id}")
     public ResponseEntity<CorteTecidoResponseDto> buscarPorId(@PathVariable Integer id) {
         CorteTecidoResponseDto corteTecido = CorteTecidoMapper.toResponseDto(
@@ -48,6 +76,15 @@ public class CorteTecidoController {
         return ResponseEntity.status(200).body(corteTecido);
     }
 
+    @Operation(summary = "Atualização de Corte de Tecido.", description = "Retorna um objeto CorteTecidoResponseDto atualizado com os valores de um CorteTecidoRequestDto.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Registro atualizado com sucesso."),
+            @ApiResponse(responseCode = "404", description = "Nenhum registro com o ID informado no PathVariable foi encontrado."),
+    })
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "Objeto do tipo CorteTecidoRequestDto com valores de atualização.",
+            required = true
+    )
     @PutMapping("/{id}")
     public ResponseEntity<CorteTecidoResponseDto> atualizar(
             @PathVariable Integer id,
@@ -60,6 +97,11 @@ public class CorteTecidoController {
         return ResponseEntity.status(200).body(corteAtualizado);
     }
 
+    @Operation(summary = "Deleção de um registro de corte de tecido.", description = "Não possui retorno de corpo quando o registro é deletado.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Sem corpo de resposta, registro deletado com sucesso."),
+            @ApiResponse(responseCode = "404", description = "Nenhum registro com o ID informado no PathVariable foi encontrado."),
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletar(@PathVariable Integer id) {
         corteTecidoService.deletarCorteTecido(id);
