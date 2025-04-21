a2bdb5f00c08e09aab99a9256683fefc8b31a44e
guiduartec <guilherme.cabral@sptech.school>
2025-04-21 03:16:37 -0300

    Feature: Implementação do JWT/Login/Segurança
    Implementei o JWT de acordo com a documentação postada pelo professor. A aplicação ainda não foi testada, mas aparentemente está correta.

---
+++ API_CRUDBackend/src/main/java/school/sptech/CRUDBackend/controller/controllerLogin/FuncionarioControllerLogin.java
@@ -0,0 +1,50 @@
+package school.sptech.CRUDBackend.controller.controllerLogin;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import school.sptech.CRUDBackend.dto.dtoLogin.*;
+import school.sptech.CRUDBackend.entity.Funcionario;
+import school.sptech.CRUDBackend.service.serviceLogin.FuncionarioServiceLogin;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/funcionarios")
+public class FuncionarioControllerLogin {
+    @Autowired
+    private FuncionarioServiceLogin funcionarioServiceLogin;
+
+    @PostMapping
+    @SecurityRequirement(name = "Bearer")
+    public ResponseEntity<Void> criar(@RequestBody @Valid FuncionarioCriacaoDto funcionarioCriacaoDto) {
+
+        final Funcionario novoFuncionario = FuncionarioMapper.of(funcionarioCriacaoDto);
+        this.funcionarioServiceLogin.criar(novoFuncionario);
+        return ResponseEntity.status(201).build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<FuncionarioTokenDto> login(@RequestBody FuncionarioLoginDto funcionarioLoginDto) {
+
+        final Funcionario usuario = FuncionarioMapper.of(funcionarioLoginDto);
+        FuncionarioTokenDto funcionarioTokenDto = this.funcionarioServiceLogin.autenticar(usuario);
+
+        return ResponseEntity.status(200).body(funcionarioTokenDto);
+    }
+
+    @GetMapping
+    @SecurityRequirement(name = "Bearer")
+    public ResponseEntity<List<FuncionarioListarDto>> listarTodos() {
+
+        List<FuncionarioListarDto> usuariosEncontrados = this.funcionarioServiceLogin.listarTodos();
+
+        if (usuariosEncontrados.isEmpty()){
+            return ResponseEntity.status(204).build();
+        }
+        return ResponseEntity.status(200).body(usuariosEncontrados);
+
+    }
+}
--- API_CRUDBackend/src/main/java/school/sptech/CRUDBackend/repository/FuncionarioRepository.java
+++ API_CRUDBackend/src/main/java/school/sptech/CRUDBackend/repository/FuncionarioRepository.java
@@ -3,6 +3,9 @@ package school.sptech.CRUDBackend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import school.sptech.CRUDBackend.entity.Funcionario;
 
+import java.util.Optional;
+
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer> {
+    Optional<Funcionario> findByEmail(String email);
     Boolean existsByCpfOrEmail(String cpf, String email);
 }
