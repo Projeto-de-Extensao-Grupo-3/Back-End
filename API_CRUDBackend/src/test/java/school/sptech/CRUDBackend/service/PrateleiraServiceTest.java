d3202d03e1d30277329a95103de5b98b91c4a454
JoaoRossii <joao.rossi@sptech.school>
2025-06-06 18:40:29 -0300

    testes de prateleira finalizados

--- API_CRUDBackend/src/test/java/school/sptech/CRUDBackend/service/PrateleiraServiceTest.java
+++ API_CRUDBackend/src/test/java/school/sptech/CRUDBackend/service/PrateleiraServiceTest.java
@@ -9,13 +9,16 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import school.sptech.CRUDBackend.entity.ConfeccaoRoupa;
 import school.sptech.CRUDBackend.entity.Prateleira;
 import school.sptech.CRUDBackend.exception.Prateleira.PrateleiraConflitoException;
+import school.sptech.CRUDBackend.exception.Prateleira.PrateleiraNaoEncontradaException;
 import school.sptech.CRUDBackend.repository.PrateleiraRepository;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -81,5 +84,95 @@ class PrateleiraServiceTest {
 
         assertNotNull(resultado);
         assertTrue(resultado.isEmpty());
+    }
+
+    @Test
+    void deveRetornarPrateleiraPorIdValido() {
+        Prateleira prateleira = new Prateleira(1, "2345678");
+
+        when(repository.findById(prateleira.getIdPrateleira())).thenReturn(Optional.of(prateleira));
+
+        Prateleira resultado = service.buscarPorId(prateleira.getIdPrateleira());
+
+        assertNotNull(resultado);
+        assertEquals(1, resultado.getIdPrateleira());
+        assertEquals("2345678", resultado.getCodigo());
+    }
+
+    @Test
+    void deveFalharRetornoDePrateleiraPorIdInvalido() {
+        int idInvalido = 999;
+
+        when(repository.findById(idInvalido)).thenReturn(Optional.empty());
+
+        assertThrows(PrateleiraNaoEncontradaException.class,
+                () -> service.buscarPorId(idInvalido));
+    }
+
+    @Test
+    void deveRetornarPrateleiraPorCodigoValido() {
+        Prateleira prateleira = new Prateleira(1, "2345678");
+
+        when(repository.findByCodigo(prateleira.getCodigo())).thenReturn(Optional.of(prateleira));
+
+        Prateleira resultado = service.buscarPorCodigo(prateleira.getCodigo());
+
+        assertNotNull(resultado);
+        assertEquals(1, resultado.getIdPrateleira());
+        assertEquals("2345678", resultado.getCodigo());
+    }
+
+    @Test
+    void deveFalharRetornoDePrateleiraPorCodigoInvalido() {
+        String codigoInvalido = "";
+
+        when(repository.findByCodigo(codigoInvalido)).thenReturn(Optional.empty());
+
+        assertThrows(PrateleiraNaoEncontradaException.class,
+                () -> service.buscarPorCodigo(codigoInvalido));
+    }
+
+    @Test
+    void deveAtualizarPrateleiraPorIdValido() {
+        Prateleira prateleira = new Prateleira(1, "2345678");
+
+        when(repository.existsById(prateleira.getIdPrateleira())).thenReturn(true);
+        prateleira.setIdPrateleira(1);
+        prateleira.setCodigo("8765432");
+        Prateleira resultado = service.atualizarPorId(prateleira.getIdPrateleira(), prateleira);
+
+        assertNotNull(resultado);
+        assertEquals(1, resultado.getIdPrateleira());
+        assertEquals("8765432", resultado.getCodigo());
+    }
+
+    @Test
+    void deveFalharAoAtualizarPrateleiraComIdInvalido() {
+        Prateleira prateleira = new Prateleira(1, "2345678");
+
+        when(repository.existsById(prateleira.getIdPrateleira())).thenReturn(false);
+
+        assertThrows(PrateleiraNaoEncontradaException.class,
+                () -> service.atualizarPorId(prateleira.getIdPrateleira(), prateleira));
+    }
+
+    @Test
+    void deveRemoverPorIdQuandoIdValido() {
+        int id = 1;
+
+        when(repository.existsById(id)).thenReturn(true);
+
+        service.removerPorId(id);
+        verify(repository).deleteById(id);
+    }
+
+    @Test
+    void deveFalharAoRemoverPorIdQuandoIdInvalido() {
+        int id = 999;
+
+        when(repository.existsById(id)).thenReturn(false);
+
+        assertThrows(PrateleiraNaoEncontradaException.class,
+                () -> service.removerPorId(id));
     }
 }
