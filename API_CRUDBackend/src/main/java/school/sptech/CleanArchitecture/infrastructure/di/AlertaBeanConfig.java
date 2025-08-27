9743ebdc51d539f185af4a51826e40c17475905c
euFernandoAmorim <fernandoalmeida.mda@gmail.com>
2025-08-27 20:26:35 -0300

    "AlertaBeanConfig com problemas de bean"

---
+++ API_CRUDBackend/src/main/java/school/sptech/CleanArchitecture/infrastructure/di/AlertaBeanConfig.java
@@ -0,0 +1,16 @@
+package school.sptech.CleanArchitecture.infrastructure.di;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import school.sptech.CleanArchitecture.core.application.usecase.CriarAlertaUseCase;
+import school.sptech.CleanArchitecture.infrastructure.persistence.jpa.alerta.AlertaAdapter;
+
+@Configuration
+public class AlertaBeanConfig {
+
+    @Bean
+    public CriarAlertaUseCase criarAlertaUseCase(AlertaAdapter alertaAdapter){
+        return new CriarAlertaUseCase(alertaAdapter);
+    }
+
+}
---
+++ API_CRUDBackend/src/main/java/school/sptech/CleanArchitecture/infrastructure/persistence/jpa/alerta/AlertaEntityMapper.java
@@ -0,0 +1,39 @@
+package school.sptech.CleanArchitecture.infrastructure.persistence.jpa.alerta;
+
+import school.sptech.CleanArchitecture.core.domain.entity.Alerta;
+
+import java.util.Objects;
+
+public class AlertaEntityMapper {
+
+    public static AlertaEntity ofDomain (Alerta domain){
+        if (Objects.isNull(domain)) {
+            return null;
+        }
+
+        var enitity = new AlertaEntity();
+
+        enitity.setIdAlerta(domain.getIdAlerta());
+        enitity.setDescricao(domain.getDescricao());
+        enitity.setDataHora(domain.getDataHora());
+        enitity.setItemEstoque(domain.getItemEstoque());
+
+        return enitity;
+    }
+
+    public static Alerta ofEntity(AlertaEntity entity) {
+        if (Objects.isNull(entity)) {
+            return null;
+        }
+
+        var domain = new Alerta();
+
+        domain.setIdAlerta(entity.getIdAlerta());
+        domain.setDescricao(entity.getDescricao());
+        domain.setDataHora(entity.getDataHora());
+        domain.setItemEstoque(entity.getItemEstoque());
+
+        return domain;
+    }
+
+}
