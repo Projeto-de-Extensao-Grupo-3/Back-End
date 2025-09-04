cb8189eed1285aaca4ce1fa66662196a3558ba25
JoaoRossii <joao.rossi@sptech.school>
2025-09-04 00:14:32 -0300

    Refatoração de Parceiro: Core Completo, finalizei persistence jpa

--- API_CRUDBackend/src/main/java/school/sptech/CleanArchitecture/infrastructure/persistence/jpa/parceiro/ParceiroAdapter.java
+++ API_CRUDBackend/src/main/java/school/sptech/CleanArchitecture/infrastructure/persistence/jpa/parceiro/ParceiroAdapter.java
@@ -1,4 +1,73 @@
 package school.sptech.CleanArchitecture.infrastructure.persistence.jpa.parceiro;
 
-public class ParceiroAdapter {
+import school.sptech.CleanArchitecture.core.adapters.ParceiroGateway;
+import school.sptech.CleanArchitecture.core.domain.entity.Parceiro;
+
+import java.util.List;
+
+public class ParceiroAdapter implements ParceiroGateway {
+
+    private final ParceiroRepository parceiroRepository;
+
+    public ParceiroAdapter(ParceiroRepository parceiroRepository) {
+        this.parceiroRepository = parceiroRepository;
+    }
+
+    @Override
+    public Parceiro save(Parceiro parceiro) {
+        ParceiroEntity parceiroEntity = ParceiroEntityMapper.ofDomain(parceiro);
+        ParceiroEntity parceiroSalvarNoBanco = parceiroRepository.save(parceiroEntity);
+        return ParceiroEntityMapper.ofEntity(parceiroSalvarNoBanco);
+    }
+
+    @Override
+    public boolean existsById(Integer id) {
+        return parceiroRepository.existsById(id);
+    }
+
+    @Override
+    public Parceiro findById(Integer id) {
+        ParceiroEntity parceiro = parceiroRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Parceiro com ID " + id + " não encontrado"));
+        return ParceiroEntityMapper.ofEntity(parceiro);
+    }
+
+    @Override
+    public List<Parceiro> findAll() {
+        return parceiroRepository.findAll()
+                .stream()
+                .map(ParceiroEntityMapper::ofEntity)
+                .toList();
+    }
+
+    @Override
+    public Parceiro findByNome(String nome) {
+        return ParceiroEntityMapper.ofEntity(parceiroRepository.findByNome(nome));
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        parceiroRepository.deleteById(id);
+    }
+
+    @Override
+    public Boolean existsByEmailOrIdentificacaoOrEnderecoAllIgnoreCase(String email, String identificacao, String endereco) {
+        return parceiroRepository.existsByEmailOrIdentificacaoOrEnderecoAllIgnoreCase(email, identificacao, endereco);
+    }
+
+    @Override
+    public List<Parceiro> findByCategoria(String categoria) {
+        return parceiroRepository.findByCategoria(categoria)
+                .stream()
+                .map(ParceiroEntityMapper::ofEntity)
+                .toList();
+    }
+
+    @Override
+    public List<Parceiro> findByCategoriaAndNomeContainsIgnoreCase(String categoria, String nome) {
+        return parceiroRepository.findByCategoriaAndNomeContainsIgnoreCase(categoria, nome)
+                .stream()
+                .map(ParceiroEntityMapper::ofEntity)
+                .toList();
+    }
 }
--- API_CRUDBackend/src/main/java/school/sptech/CleanArchitecture/infrastructure/persistence/jpa/parceiro/ParceiroEntityMapper.java
+++ API_CRUDBackend/src/main/java/school/sptech/CleanArchitecture/infrastructure/persistence/jpa/parceiro/ParceiroEntityMapper.java
@@ -1,4 +1,29 @@
 package school.sptech.CleanArchitecture.infrastructure.persistence.jpa.parceiro;
 
+import school.sptech.CleanArchitecture.core.domain.entity.Parceiro;
+import school.sptech.CleanArchitecture.core.domain.valueObject.EmailVo;
+
 public class ParceiroEntityMapper {
+
+    public static Parceiro ofEntity(ParceiroEntity entity) {
+        Parceiro domain = new Parceiro();
+        domain.setId(entity.getIdParceiro());
+        domain.setCategoria(entity.getCategoria());
+        domain.setNome(entity.getNome());
+        domain.setTelefone(entity.getTelefone());
+        new EmailVo(entity.getEmail());
+        domain.setIdentificacao(entity.getIdentificacao());
+        return domain;
+    }
+
+    public static ParceiroEntity ofDomain(Parceiro domain) {
+        ParceiroEntity entity = new ParceiroEntity();
+        entity.setIdParceiro(domain.getId());
+        entity.setCategoria(domain.getCategoria());
+        entity.setNome(domain.getNome());
+        entity.setTelefone(domain.getTelefone());
+        entity.setEmail(domain.getEmail().getValue());
+        entity.setIdentificacao(domain.getIdentificacao());
+        return entity;
+    }
 }
