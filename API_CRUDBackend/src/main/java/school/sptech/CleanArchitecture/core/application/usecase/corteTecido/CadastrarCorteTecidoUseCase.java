44ecfe70e730b5424bbe439caa66a7044abcfb8e
JoaoRossii <joao.rossi@sptech.school>
2025-09-08 16:02:25 -0300

    refatoraçaão de corte tecido iniciada, falta a parte de infra

--- API_CRUDBackend/src/main/java/school/sptech/CleanArchitecture/core/application/usecase/corteTecido/CadastrarCorteTecidoUseCase.java
+++ API_CRUDBackend/src/main/java/school/sptech/CleanArchitecture/core/application/usecase/corteTecido/CadastrarCorteTecidoUseCase.java
@@ -1,4 +1,25 @@
 package school.sptech.CleanArchitecture.core.application.usecase.corteTecido;
 
+import school.sptech.CleanArchitecture.core.adapters.CorteTecidoGateway;
+import school.sptech.CleanArchitecture.core.application.command.corteTecido.CriarCorteTecidoCommand;
+import school.sptech.CleanArchitecture.core.domain.entity.CorteTecido;
+
 public class CadastrarCorteTecidoUseCase {
+
+    private final CorteTecidoGateway gateway;
+
+    public CadastrarCorteTecidoUseCase(CorteTecidoGateway gateway) {
+        this.gateway = gateway;
+    }
+
+    public CorteTecido executar(CriarCorteTecidoCommand command){
+        var corteTecidoParaRegistrar = new CorteTecido();
+
+        corteTecidoParaRegistrar.setInicio(command.inicio());
+        corteTecidoParaRegistrar.setTermino(command.temino());
+        corteTecidoParaRegistrar.setFuncionario(command.funcionario());
+        corteTecidoParaRegistrar.setLoteItemEstoque(command.loteItemEstoque());
+
+        return gateway.save(corteTecidoParaRegistrar);
+    }
 }
--- API_CRUDBackend/src/main/java/school/sptech/CleanArchitecture/core/application/usecase/corteTecido/DeletarCorteTecidoPorIdUseCase.java
+++ API_CRUDBackend/src/main/java/school/sptech/CleanArchitecture/core/application/usecase/corteTecido/DeletarCorteTecidoPorIdUseCase.java
@@ -1,4 +1,20 @@
 package school.sptech.CleanArchitecture.core.application.usecase.corteTecido;
 
+import school.sptech.CRUDBackend.exception.corteTecido.CorteTecidoNaoEncontradoException;
+import school.sptech.CleanArchitecture.core.adapters.CorteTecidoGateway;
+
 public class DeletarCorteTecidoPorIdUseCase {
+
+    private final CorteTecidoGateway gateway;
+
+    public DeletarCorteTecidoPorIdUseCase(CorteTecidoGateway gateway) {
+        this.gateway = gateway;
+    }
+
+    public void executar(Integer id){
+        if(gateway.existsById(id)){
+            gateway.deleteById(id);
+        }
+        throw new CorteTecidoNaoEncontradoException("Corte de tecido não encontrado");
+    }
 }
