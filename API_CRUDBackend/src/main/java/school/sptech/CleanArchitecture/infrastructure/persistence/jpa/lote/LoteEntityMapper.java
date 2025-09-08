490f7c6c59537d45f64417fe23f02631a50cd62c
JoaoRossii <joao.rossi@sptech.school>
2025-09-08 17:56:43 -0300

    ajuste de LoteEntityMapper

--- API_CRUDBackend/src/main/java/school/sptech/CleanArchitecture/infrastructure/persistence/jpa/lote/LoteEntityMapper.java
+++ API_CRUDBackend/src/main/java/school/sptech/CleanArchitecture/infrastructure/persistence/jpa/lote/LoteEntityMapper.java
@@ -3,6 +3,8 @@ package school.sptech.CleanArchitecture.infrastructure.persistence.jpa.lote;
 import school.sptech.CleanArchitecture.core.domain.entity.Funcionario;
 import school.sptech.CleanArchitecture.core.domain.entity.Lote;
 import school.sptech.CleanArchitecture.core.domain.entity.Parceiro;
+import school.sptech.CleanArchitecture.infrastructure.persistence.jpa.funcionario.FuncionarioEntity;
+import school.sptech.CleanArchitecture.infrastructure.persistence.jpa.parceiro.ParceiroEntity;
 
 public class LoteEntityMapper {
 
@@ -13,24 +15,41 @@ public class LoteEntityMapper {
         domain.setDataEntrada(entity.getDataEntrada());
         if (entity.getParceiro() != null) {
             Parceiro parceiro = new Parceiro();
-            parceiro.setId(entity.getParceiro());
+            parceiro.setId(entity.getParceiro().getIdParceiro();
             domain.setParceiro(parceiro);
         }
         if (entity.getResponsavel() != null) {
             Funcionario funcionario = new Funcionario();
-            funcionario.setIdFuncionario(entity.getResponsavel());
+            funcionario.setIdFuncionario(entity.getResponsavel().getIdFuncionario());
             domain.setResponsavel(funcionario);
         }
         return domain;
     }
 
     public static LoteEntity ofDomain(Lote domain) {
+        ParceiroEntity parceiroEntity = new ParceiroEntity();
+        parceiroEntity.setIdParceiro(domain.getParceiro().getId());
+        parceiroEntity.setCategoria(domain.getParceiro().getCategoria());
+        parceiroEntity.setNome(domain.getParceiro().getNome());
+        parceiroEntity.setTelefone(domain.getParceiro().getTelefone());
+        parceiroEntity.setEmail(domain.getParceiro().getEmail().getValue());
+        parceiroEntity.setEndereco(domain.getParceiro().getEndereco());
+        parceiroEntity.setIdentificacao(domain.getParceiro().getIdentificacao());
+
+        FuncionarioEntity funcionarioEntity = new FuncionarioEntity();
+        funcionarioEntity.setIdFuncionario(domain.getResponsavel().getIdFuncionario());
+        funcionarioEntity.setNome(domain.getResponsavel().getNome());
+        funcionarioEntity.setCpf(domain.getResponsavel().getCpf().getValue());
+        funcionarioEntity.setTelefone(domain.getResponsavel().getTelefone().getValue());
+        funcionarioEntity.setEmail(domain.getResponsavel().getEmail().getValue());
+        funcionarioEntity.setSenha(domain.getResponsavel().getSenha());
+
         LoteEntity entity = new LoteEntity();
         entity.setIdLote(domain.getIdLote());
         entity.setDescricao(domain.getDescricao());
         entity.setDataEntrada(domain.getDataEntrada());
-        entity.setParceiro(domain.getParceiro().getId());
-        entity.setResponsavel(domain.getResponsavel().getIdFuncionario());
+        entity.setParceiro(parceiroEntity);
+        entity.setResponsavel(funcionarioEntity);
         return entity;
     }
 }
