a34b164ba16fa9bf116a6b9a2ef5732d391b1662
GiorgioLA <giorgio.antunes@sptech.school>
2025-12-01 03:23:12 -0300

    Alterações nos endpoints de dashboard para funcionamento correto e teste com CICD

--- API_CRUDBackend/src/main/java/school/sptech/CleanArchitecture/infrastructure/persistence/jpa/loteItemEstoque/LoteItemEstoqueRepository.java
+++ API_CRUDBackend/src/main/java/school/sptech/CleanArchitecture/infrastructure/persistence/jpa/loteItemEstoque/LoteItemEstoqueRepository.java
@@ -42,20 +42,20 @@ public interface LoteItemEstoqueRepository extends JpaRepository<LoteItemEstoque
     Long contarTotalSaida();
 
     @Query(value = """
-    SELECT 
-        img.url AS url,
-        ie.descricao AS nomeItem,
-        se.qtd_saida AS qtdItem,
-        l.id_lote AS idLote,
-        p.nome AS nomeParceiro,
-        TIMESTAMP(CONCAT(se.data, ' ', se.hora)) AS saidaEstoque
-    FROM lote_item_estoque lie
-    JOIN item_estoque ie ON ie.id_item_estoque = lie.fk_item_estoque
-    LEFT JOIN imagem img ON img.id_imagem = ie.fk_imagem
-    JOIN lote l ON l.id_lote = lie.fk_lote
-    JOIN parceiro p ON p.id_parceiro = l.fk_parceiro
-    JOIN saida_estoque se ON se.fk_lote_item_estoque = lie.id_lote_item_estoque
-    ORDER BY se.data DESC, se.hora DESC, se.id_saida_estoque
+            SELECT\s
+                img.url AS url,
+                ie.descricao AS nomeItem,
+                se.qtd_saida AS qtdItem,
+                l.id_lote AS idLote,
+                p.nome AS nomeParceiro,
+                TIMESTAMP(CONCAT(se.data, ' ', se.hora)) AS saidaEstoque
+            FROM lote_item_estoque lie
+            JOIN item_estoque ie ON ie.id_item_estoque = lie.fk_item_estoque
+            LEFT JOIN imagem img ON img.id_imagem = ie.fk_imagem
+            JOIN lote l ON l.id_lote = lie.fk_lote
+            JOIN saida_estoque se ON se.fk_lote_item_estoque = lie.id_lote_item_estoque
+            left JOIN parceiro p ON p.id_parceiro = se.fk_costureira\s
+            ORDER BY se.data DESC, se.hora DESC, se.id_saida_estoque
     LIMIT :limit OFFSET :offset
     """, nativeQuery = true)
     List<Object[]> buscarSaidaPaginada(@Param("offset") int offset, @Param("limit") int limit);
